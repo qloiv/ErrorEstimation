3760067455fdc6a8fc91acab0d029a5f347e2097
rs-linux <rs-linux@iff.inter.de>
2017-08-14 14:25:05 +0200

    add class transformation Object

---
+++ TransformationObject.m
@@ -0,0 +1,64 @@
+classdef TransformationObject
+    %TRANSFORMATIONOBJECT Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        sampleSize_1
+        sampleSize_2       
+        start_pt
+        end_pt
+        angles
+        arms
+        
+        original_pts
+        transformed_pts
+        error
+        trajectory
+    end
+    
+    methods
+        function obj = TransformationObject(start_end, sampleSize_1, sampleSize_2,angles,arms)
+            obj.start_pt,obj.end_pt = start_end
+            obj.sampleSize_1 = sampleSize_1
+            obj.sampleSize_2 = sampleSize_2
+            obj.angles = angles
+            obj.arms = arms
+        end
+        function obj = toJSpace(obj)
+            obj.original_pts = sample(obj.start_pt,obj.end_pt,-1,obj.sampleSize_1); %sample points from line
+            obj.transformed_pts = (transform(obj.original_pts, obj.angles,obj.arms)) %to joint space
+        end
+        function obj = trajGen(obj)
+            %build trajectory from transformed points
+            obj.trajectory = (trajectory1(obj.transformed_pts,obj.sampleSize_2)) %in joint space
+        end
+        function obj = toTSpace(obj)
+            obj.transformed_pts = retransform(obj.arms,obj.trajectory) %from joint space to 
+        end
+        function obj = computeError(obj)
+            ori_pts = []
+            ori_pts(1,:) = sampleMultiple(obj.original_pts(1,:), obj.sampleSize_2)
+            ori_pts(2,:) = sampleMultiple(obj.original_pts(2,:),obj.sampleSize_2)
+
+            trs_pts = pts
+            trs_pts(3,:) = []
+            obj.error = mse(trs_pts,ori_pts)
+        end
+        function plot(obj)
+            
+            figure
+            ax1 = subplot(2,1,1);
+            plot(err)
+            title(ax1,'Error')
+
+            ax2 = subplot(2,1,2);
+            plot(obj.transformed_pts(1,:),obj.transformed_pts(2,:));
+            hold on;
+            plot(obj.original_pts(1,:),obj.original_pts(2,:))
+            title(ax2,'Original and Transformed Points')
+            hold off;
+       end
+    
+    end
+end
+
