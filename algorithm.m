16cffaef32a80271b333f25e101f4b4f94bc34a3
rs-linux <rs-linux@iff.inter.de>
2017-08-23 13:28:29 +0200

    Change sample in algorithm to smallest increase (double sample points)

--- algorithm.m
+++ algorithm.m
@@ -2,7 +2,6 @@ error_rate = 0.0001;
 sSize1 = 2;
 sSize2 = 20;
 angles_ori = zeros(1,sSize1);
-i = 2;
 angles = sampleMultiple(angles_ori,i);
 while(1)
 t_obj = TransformationObject([0;0],[0;1],sSize1, sSize2, angles,[1,1,1]);
@@ -14,7 +13,6 @@ t_obj.plot()
 if(t_obj.meanError <= error_rate)
     break
 end
-i = i+1;
 angles = sampleMultiple(angles, 3);
 sSize1 = size(angles,2)
 end
